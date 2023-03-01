66ebbfb2dfeb11362c665c1bd29d323ccda4a7cc
user <1253417392@qq.com>
2023-03-01 16:25:53 +0800

    测试下送审账号登录

--- test_case02.py
+++ test_case02.py
@@ -9,21 +9,21 @@ def reqHttp(url, data, headers):
 
 class Test_login:
     def test_01(self):
-        url = "http://192.168.122.176:9010/hc/app/noAuth/logon/doRegister"
-        data = {"promotionChannels": "googlePlay",
-                "password": "",
-                "flag": "01",
-                "phone": "2221591217",
-                "countryCode": "52",
-                "shortNo": "9999"}
+        url = "https://app.hinance.online/hc/app/noAuth/logon/login"
+        data = {
+            "promotionChannels": "googlePlay",
+            "password": "",
+            "flag": 1,
+            "phone": "8888888881",
+            "shortNo": "9999"
+        }
         header = {
             'app-name': 'Hinance',
             'app-version': '1.0.7',
             'channel': 'googlePlay',
-            'commercialId': "1",
+            'commercialId': '01',
             'lang': 'zh',
             'organizationId': 'DCMEX',
-            'token': '',
             'Content-Type': 'application/json'
         }
         res = reqHttp(url, data, header)
